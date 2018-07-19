7008ce407129de1c6617d26cddfbd592106d3390
nakimera <prossienakimera@gmail.com>
2018-07-19 08:09:32 +0300

    add entryid to Entry

--- app/api/v1/entry/models.py
+++ app/api/v1/entry/models.py
@@ -1,6 +1,7 @@
 class Entry:
 
-    def __init__(self, date, title, entry):
+    def __init__(self, entryId, date, title, entry):
+        self.entryId = entryId
         self.date = date
         self.title = title
         self.entry = entry
