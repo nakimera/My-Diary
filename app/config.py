0cc282b6755f1e92bd541a969f3efdffcd32e4ec
nakimera <prossienakimera@gmail.com>
2018-07-19 07:30:22 +0300

    correct indentation

--- app/config.py
+++ app/config.py
@@ -1,21 +1,21 @@
 class Config:
-    SECRET-KEY = " "
+    SECRET_KEY = "TcQsWISFjRG4243XobHPIaDxMioisOba"
     DEBUG = False
-    TESTING = False
-    DEVELOPEMENT = False
+    TESTING = False 
+    DEVELOPMENT = False
 
-    class DevelopmentConfig(Config):
-        DEBUG = True
-        DEVELOPEMENT = True
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
 
-    class TestingConfig(Config):
-        TESTING = True
+class TestingConfig(Config):
+    TESTING = True
 
-    class ProductionConfig(Config):
-        pass
-
-    configuration = {
-        "development" : DevelopmentConfig,
-        "testing" : TestingConfig,
-        "production" :ProductionConfig
-    }
+class ProductionConfig(Config):
+    pass
+    
+configuration = {
+    "development" : DevelopmentConfig,
+    "testing" : TestingConfig,
+    "production" : ProductionConfig
+}
