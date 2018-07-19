1bf2ce813d286dbf811ffcb988e8f0081d75798d
nakimera <prossienakimera@gmail.com>
2018-07-19 08:11:19 +0300

    add url prefix to user blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,9 @@ def create_app(environment):
     app.config.from_object(configuration[environment])
     
     from app.api.v1.entry.views import mod as entry
+    from app.api.v1.user.views import mod as user
 
     app.register_blueprint(entry, url_prefix='/api/v1/users/entries')
+    app.register_blueprint(entry, url_prefix='/api/v1/users')
 
     return app
