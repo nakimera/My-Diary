a0a3958bdc111eeaead3c4a02f7ef5f490328c75
nakimera <prossienakimera@gmail.com>
2018-07-19 08:57:14 +0300

    add user endpoint

--- app/api/v1/user/models.py
+++ app/api/v1/user/models.py
@@ -1,9 +1,14 @@
 import uuid
 
+users = []
+
 class User(object):
 
-    def __init__(self, id, username, emailAddress, password):
-        self.id = int(uuid.uuid3)
+    def __init__(self, username, emailAddress, password):
+        self.id = int(uuid.uuid1())
         self.username = username
         self.emailAddress= emailAddress
         self.password = password
+
+    def add_user(user):
+        users.append(user)
--- app/api/v1/user/views.py
+++ app/api/v1/user/views.py
@@ -1,3 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app.api.v1.user.models import User
 
 mod = Blueprint('user', __name__)
+
+
+@mod.route('/', methods=['POST', 'GET'])
+def users():
+
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get("username", None)
+        emailAddress = data.get("emailAddress", None)
+        password = data.get("password", None)
+        user = User(username, emailAddress, password)
+        user.add_user()
+        return jsonify({
+                "message": "User sucessfully created",
+                "status": True,
+                "data": {
+                    "id" : user.id,
+                    "username" : "{}".format(username),
+                    "emailAddress" : "{}".format(emailAddress)
+                    }
+                }), 201
+
