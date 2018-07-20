3150b08c025df89f5b59b18df9e5ae6c29c04846
nakimera <prossienakimera@gmail.com>
2018-07-20 23:19:47 +0300

    user login

--- app/api/v1/auth/views.py
+++ app/api/v1/auth/views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 import jwt
 import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.api.v1.auth.models import User
+import app.config
 
 mod = Blueprint('auth', __name__)
 
@@ -49,6 +50,7 @@ def users():
 
         for user in users_list:
             user = dict([
+                    ('id', user.id),
                     ('username', user.username),
                     ('password', user.password),
                     ('emailAddress', user.emailAddress)
@@ -59,3 +61,36 @@ def users():
             "message" : "All users successfully retrieved",
             "users" : all_users,
             }), 200
+
+@mod.route('/login', methods=['POST'])
+def login_user(): 
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Please fill all fields', 401, {"WWW-Authenticate" : 'Basic realm="Login required!"'})
+    
+
+
+    for user in users_list:
+        if not user:
+            return make_response('could not verify', 401, {"WWW-Authenticate" : 'Basic realm="Login required!"'})
+
+        if check_password_hash(user.password, auth.password):
+            token = jwt.encode(
+                {
+                    'id' : user.id, 
+                    'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+                }, 
+                app.config.('SECRET_KEY')
+            )
+            return jsonify({"token" : token})
+
+        # data = request.get_json()
+        # if data.get('username') == user.username and data.get('password_hash') == user.password:
+        #     return jsonify({
+        #         "message": "{} logged in". format(user.username),
+        #         "data" : user.id,
+        #         "status": False
+        #                 }), 200        
+    
+    return make_response('Could not verify', 401, {"WWW-Authenticate" : 'Basic realm="Login required!"'})
