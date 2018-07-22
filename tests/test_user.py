9072556a0a877ccb9b006151133e6fec62dcb1be
nakimera <prossienakimera@gmail.com>
2018-07-22 07:53:16 +0300

    test entry creation

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,13 @@
 import unittest
 from app.api.v1.auth.models import User
 
+
 class UserTests(unittest.TestCase):
+
+    # def test_index(self):
+    #     tester = app.test_client(self)
+    #     response = tester.get('/login', content_type='html/text')
+    #     self.assertAlmostEqual(response,status_code, 200)
 
     def test_user_creation(self):
         user = User('me', 'prossienakimera@gmail.com', 'pr055y')
