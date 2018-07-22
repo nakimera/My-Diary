c6a1488a6cc47dc71dfa169413eff1b2eda84b6d
nakimera <prossienakimera@gmail.com>
2018-07-22 06:49:51 +0300

    test entry file

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,13 +1,13 @@
 import unittest
-from app.api.v1.user.models import User
+from app.api.v1.auth.models import User
 
-class MyTests(unittest.TestCase):
+class UserTests(unittest.TestCase):
 
     def test_user_creation(self):
         user = User('me', 'prossienakimera@gmail.com', 'pr055y')
         assert user
-        #self.assertEqual(user.username, 'prossie', msg="username should be prossie")
-        assert user.username is type(str) is not 
+    
+
 
 if __name__ == '__main__':
     unittest.main()
