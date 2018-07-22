061958e85646076d8c693b748804a3f8aa99eb1e
nakimera <prossienakimera@gmail.com>
2018-07-22 17:47:15 +0300

    test User instance creation and user attributes

--- tests/test_user.py
+++ tests/test_user.py
@@ -12,11 +12,13 @@ class UserTests(unittest.TestCase):
     def test_user_creation(self):
         """GIVEN a User model
         WHEN a new user is created
-        THEN check the username, email_address, password
+        THEN check the user instance is created, username, email_address, password
         """
 
-        user = User('me', 'prossienakimera@gmail.com', 'pr055y')
+        user = User('prossie', 'prossienakimera@gmail.com', 'pr055y')
         assert user
+        assert user.username == 'prossie'
+        assert user.email_address == 'prossienakimera@gmail.com'
     
 
 
