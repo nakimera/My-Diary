370976ac61b8b2757fc2029ca813125023a4b4ff
nakimera <prossienakimera@gmail.com>
2018-07-25 13:03:03 +0300

    change port

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from app import create_app
 
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
+port = int(os.environ.get("PORT",5000))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=port)
