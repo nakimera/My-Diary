54fa0b19e2c4020f37382cfa634e7942353c0b01
nakimera <prossienakimera@gmail.com>
2018-07-29 17:06:44 +0300

    user model

--- app/api/v1/auth/models.py
+++ app/api/v1/auth/models.py
@@ -1,6 +1,12 @@
 import uuid
+from app import db
 
-class User(object):
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email_address = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(30))
+
 
     def __init__(self, username, email_address, password):
         self.id = int(uuid.uuid1())
