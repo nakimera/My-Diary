4ee8c6db24513a1c2e48c194f828b58d4cf7cde0
nakimera <prossienakimera@gmail.com>
2018-07-29 17:14:00 +0300

    login and signup routes

--- app/api/v1/auth/views.py
+++ app/api/v1/auth/views.py
@@ -4,42 +4,21 @@ from app.api.v1.auth.models import User
 
 mod = Blueprint('auth', __name__)
 
-users_list = []
 
-
-@mod.route('/', methods=['POST', 'GET'])
-def users():
-
+@mod.route('/signup', methods=['POST'])
+def signup():
     if request.method == 'POST':
         data = request.get_json()
         username = data.get("username", None)
         email_address = data.get("email_address", None)
         password = generate_password_hash(data.get("password", None), method='sha256')
         user = User(username, email_address, password)
-        users_list.append(user)
-        return jsonify({
-            "message": "User sucessfully created",
-            "status": True,
-            "data": {
-                "id": user.id,
-                "username": "{}".format(username),
-                "password": "{}".format(password),
-                "email_address": "{}".format(email_address),
-            }
-        }), 201
+        
+        return jsonify({"message": "User sucessfully created"}), 201
 
-    if request.method == 'GET':
-        all_users = []
+@mod.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
 
-        for user in users_list:
-            user = dict([
-                ('username', user.username),
-                ('email_address', user.email_address)
-            ])
-            all_users.append(user)
-
-        return jsonify({
-            "message": "All users successfully retrieved",
-            "users": all_users,
-        }), 200
-
+        return jsonify({'message' : '{} logged in'.format(username)}), 200
